8d39820460a411239d44914d94ef0c533bb08baa
andyhall <andy@fenomas.com>
2014-03-14 17:09:33 +0900

    trying to fix cross-site iframe error

--- edge-animate-video-sync_edgeActions.js
+++ edge-animate-video-sync_edgeActions.js
@@ -1,7 +1,7 @@
 
 (function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName){Symbol.bindSymbolAction(compId,symbolName,"creationComplete",function(sym,e){var video_code='k6wf9QjWo8w';var vid_w='560';var vid_h='315';var useIFrame=true;var site_url='http://andyhall.github.io/';$('body').css('backgroundColor','#000')
+(function(symbolName){Symbol.bindSymbolAction(compId,symbolName,"creationComplete",function(sym,e){var video_code='k6wf9QjWo8w';var vid_w='560';var vid_h='315';var useIFrame=true;var site_url='http://andyhall.github.io';$('body').css('backgroundColor','#000')
 var player;window.onVideoState=function(status){var time=player.getCurrentTime();if(status==1){sym.play(time*1000);}else if(status==2){sym.stop(time*1000);}}
 if(!useIFrame){window.onYouTubePlayerReady=function(){player=document.getElementById('player');player.addEventListener("onStateChange","onVideoState");}
 var params='?version=3&enablejsapi=1';var embedScript='<object width="'+vid_w+'" height="'+vid_h+'"><param name="movie" value="https://www.youtube.com/v/'+video_code+params+'"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed id="player" src="https://www.youtube.com/v/'+video_code+params+'" type="application/x-shockwave-flash" width="'+vid_w+'" height="'+vid_h+'" allowscriptaccess="always" allowfullscreen="true"></embed></object>';sym.$('video_holder').html(embedScript);}
